92267f012636fb10d4341145a3cb232ffad18a23
waleedikhlaq001 <waleedikhlaq564@gmail.com>
2025-02-18 14:54:03 +0500

    change route

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,6 @@ use App\Http\Controllers\Auth\ForgotPasswordController;
 |
 */
 Route::post('/forgot-password', [ForgotPasswordController::class, 'sendResetLinkEmail']);
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
